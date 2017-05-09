366af672b804d7125aa46ed51788de990fc77608
Mamunjahir <mamunjahir.com@gmail.com>
2017-05-09 15:17:59 -0400

    Final

--- app/src/main/java/info/androidhive/floatingview/FloatingViewService.java
+++ app/src/main/java/info/androidhive/floatingview/FloatingViewService.java
@@ -16,6 +16,7 @@ public class FloatingViewService extends Service {
 
     private WindowManager mWindowManager;
     private View mFloatingView;
+    //dfd
 
     public FloatingViewService() {
     }
